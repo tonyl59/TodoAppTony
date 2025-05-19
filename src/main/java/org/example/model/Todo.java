e8eff4a7c34abcb36d3059e52f3634132cac649d
tonyl59 <tony.luong59@yahoo.com>
2025-05-19 16:34:35 +0200

    Todoservice impl & modele todo finis, manque le readme et les bonus

--- src/main/java/org/example/model/Todo.java
+++ src/main/java/org/example/model/Todo.java
@@ -1,5 +1,101 @@
 package org.example.model;
 
-public class Todo {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-}
+public class Todo {
+    // Moule que je crée => Classe.
+    //Chaque tâche (ou "todo") est représentée par un objet Todo et doit obligatoirement contenir les champs suivants :
+    //
+    //id (entier) : identifiant unique généré automatiquement.
+    //name (chaîne de caractères) : le titre de la tâche.
+    //description (chaîne de caractères) : une description détaillée de la tâche.
+    //done (booléen) : un indicateur qui vaut false par défaut, puis true si la tâche est marquée comme faite.
+    //🛠️ Ces champs doivent être encapsulés dans la classe Todo, avec des getters et setters publics.
+
+    // Attributs
+    private int id;
+    private String name;
+    private String description;
+    private boolean done;
+    private static List<Todo> Todolist = new ArrayList<>();
+    private static int count = 0;
+
+    // Constructeur
+    //
+    public Todo(String name, String description) { // Marche pour la création de tache
+        count++;
+        this.id = count;
+        this.name = name;
+        this.description = description;
+        this.done = false;
+    }
+    public Todo(int id, String name, String description, boolean done) { // Marche pour la modification de tâche
+        count++;
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.done = done;
+        }
+
+    @Override
+    public String toString() {
+        String text_done;
+        if (done){
+            text_done = "Fait";
+        }else{
+            text_done = "Pas fait";
+        }
+        return String.format("Tâche id %s de %s : %s. Etat : %s", id, name, description, text_done);
+
+    }
+
+    // Setters & Getters
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public static List<Todo> getTodolist() {
+        return Todolist;
+    }
+
+    public static void setTodolist(List<Todo> todolist) {
+        Todolist = todolist;
+    }
+    }
+    // Méthodes ?
+
+
--- src/main/java/org/example/service/TodoServiceImpl.java
+++ src/main/java/org/example/service/TodoServiceImpl.java
@@ -2,12 +2,15 @@ package org.example.service;
 
 import org.example.model.Todo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TodoServiceImpl implements TodoService{
     @Override //TODO à moi de remplir toutes les methodes... stocker les todo dans qlq chose, à moi de voir où les mettre
     public List<Todo> getAll() {
-        return List.of();
+
+
+        return Todo.getTodolist();
     }
 
     @Override
@@ -17,21 +20,48 @@ public class TodoServiceImpl implements TodoService{
 
     @Override
     public void add(String name, String description) {
+        Todo todo = new Todo(name, description);
+        Todo.getTodolist().add(todo);
 
     }
 
     @Override
     public void update(int id, String name, String description, boolean done) {
+        for (Todo todo : Todo.getTodolist()){
+            if (todo.getId() == id){
+                todo.setName(name);
+                todo.setDescription(description);
+                todo.setDone(done);
+            }
+        }
+
+        //Todo.getTodolist().set(id-1, new Todo(id, name,description,done)); // Manière de faire plus propre que ecraser et refaire
+
+        //recuperer la todo qu'on veut modifier... envoyer l'id donc recuperer l'element que je veux modifier
 
     }
 
     @Override
     public void remove(int id) {
-
+        int count = 0;
+        for (Todo todo : Todo.getTodolist()){
+            if (todo.getId() == id){
+                Todo.getTodolist().remove(count);
+                break;
+            }
+            count ++;
+            }
     }
 
     @Override
     public void toggleDone(int id) {
+        for (Todo todo : Todo.getTodolist()){
+            if (todo.getId() == id){
+                todo.setDone(!todo.isDone());
 
+            }
+        }
     }
 }
+
+
